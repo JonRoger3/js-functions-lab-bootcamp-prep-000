aba3875a6a3680c643e6bef6081606c588e9f681
jonroger3@gmail.com <jonroger3@gmail.com>
2018-06-27 21:22:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,21 +6,20 @@ function happyHolidays(){
 function happyHolidaysTo(name){
   console.log ('Happy Holidays, ${name}!');
   
-  happyHolidaysTo('you');
-}
+  name === you;
+  
+ }
 
 
 function happyHolidayTo(holiday, name){
   console.log ('Happy ${holiday}, ${name}!');
-  
-  happyHolidayTo('Independence Day', 'you');
+
 }
 
 
 function holidayCountdown(days, holiday){
   console.log ('It\'s ${days} days until ${holiday}!');
   
-  holidayCountdown('20', 'Mother\'s Day');
 }
 
 
